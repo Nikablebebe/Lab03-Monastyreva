179cfad680cc2a344a9d757d61443dea457bea30
Nikablebebe <nestarberry3@gmail.com>
2026-09-21 22:22:03 +0300

    Lab03: compound assignment operators

--- Program.cs
+++ Program.cs
@@ -1,1 +1,13 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Банковский счёт");
+
+double balance = 1000;
+Console.WriteLine($"Начальный баланс: {balance}");
+
+balance += 500; // пополнение
+Console.WriteLine($"После пополнения на 500: {balance}");
+
+balance -= 200; // покупка
+Console.WriteLine($"После начисления 5% {balance}");
+
+balance /= 2; //разделили счет пополам с партнером
+Console.WriteLine($"После деления пополам: {balance}");
